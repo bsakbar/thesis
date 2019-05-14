2c52287efe098fe0284ad50452b816fcfd351a89
Batool Akbar <batool@Batools-MacBook-Pro.local>
2019-05-14 00:39:22 -0400

    add some sources and fix logo poisition

--- web/download.js
+++ web/download.js
@@ -40,7 +40,7 @@ function demoFromHTML() {
             function (dispose) {
                 // dispose: object with X, Y of the last line add to the PDF
                 //          this allow the insertion of new lines after html
-                pdf.save('Report.pdf');
+                pdf.save('Akbar_Batool_Report.pdf');
             }, margins
         );
     }
